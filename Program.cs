3ea7c3999f0fd0c2c65d1a6886fd29086f4058be
ocean <ocean@gmail.com>
2023-12-04 14:25:46 +0300

    osnovnoy block

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива, разделяя их запятыми:");
+        string input = Console.ReadLine();
+
+        string[] inputArray = input.Split(',');
+
+        string[] newArray = FilterArray(inputArray);
+
+        Console.WriteLine("Новый массив:");
+        foreach (string element in newArray)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
